f3f2d1eb60395452e34e3004b87c0ff5ad9c5c1a
Hannes333 <Johannesgysel@gmx.ch>
2017-08-21 17:06:49 +0200

    Update FDSchnittkontur.m

--- FDSchnittkontur.m
+++ FDSchnittkontur.m
@@ -2,7 +2,7 @@ function []=FDSchnittkontur(D,d,Konturen)
 %Funktion, die die Konturen darstellt
 
 if D==1
-    hold on %Schreibe folgende Grafikbefehle in das geöffnete Grafikfenster
+    hold on %Schreibe folgende Grafikbefehle in das geÃ¶ffnete Grafikfenster
     %for k=1:size(Konturen,1)
     k=d;
     for i=1:size(Konturen,2)
@@ -12,10 +12,10 @@ if D==1
     end
     %end
     axis('image'); %Skalierung der Achsen fix
-    axis tight %DarstellungsFeld so nahe am Objekt wie möglich
+    axis tight %DarstellungsFeld so nahe am Objekt wie mÃ¶glich
     %set(gcf, 'visible', 'on');
     versiontext=version;
-    if str2double(versiontext(1:3))>=7.9 %ab 2014b neue Grafikplots
+    if str2double(versiontext(1:3))>=9.1 %ab 2014b neue Grafikplots
         %set(gcf, 'color', [1 1 1]) %figure color (gcf->figure settings)
         %set(gca, 'color', [1 1 1]) %axis color (gca->axis settings)
         %set(gca, 'Clipping', 'off');
