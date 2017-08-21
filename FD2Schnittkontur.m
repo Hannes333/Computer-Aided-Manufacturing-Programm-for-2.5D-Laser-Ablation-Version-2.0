08362749b3b884dfc9399edb9f3ce194951f8913
Hannes333 <Johannesgysel@gmx.ch>
2017-08-21 17:05:26 +0200

    Update FD2Schnittkontur.m

--- FD2Schnittkontur.m
+++ FD2Schnittkontur.m
@@ -2,7 +2,7 @@ function []=FD2Schnittkontur(D,d,Konturen,RadiusMax)
 %Funktion, die die Konturen darstellt
 
 if D==1
-    hold on %Schreibe folgende Grafikbefehle in das geöffnete Grafikfenster
+    hold on %Schreibe folgende Grafikbefehle in das geÃ¶ffnete Grafikfenster
     %for k=1:size(Konturen,1)
     k=d;
     for i=1:size(Konturen,2)
@@ -13,10 +13,10 @@ if D==1
     %end
     %axis('image'); %Skalierung der Achsen fix
     daspect([1 (360/(2*pi*RadiusMax)) 1]) %Achsenskalierung von X- und Y-Achse
-    axis tight %DarstellungsFeld so nahe am 3d Objekt wie möglich
+    axis tight %DarstellungsFeld so nahe am 3d Objekt wie mÃ¶glich
     %set(gcf, 'visible', 'on');
     versiontext=version;
-    if str2double(versiontext(1:3))>=7.9 %ab 2014b neue Grafikplots
+    if str2double(versiontext(1:3))>=9.1 %ab 2014b neue Grafikplots
         %set(gcf, 'color', [1 1 1]) %figure color (gcf->figure settings)
         %set(gca, 'color', [1 1 1]) %axis color (gca->axis settings)
         %set(gca, 'Clipping', 'off');
