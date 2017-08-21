06eb0b27abd5e6733f09fe406d63f05171f69bd8
Hannes333 <Johannesgysel@gmx.ch>
2017-08-21 13:22:48 +0200

    Update FD2StlObjekt.m

--- FD2StlObjekt.m
+++ FD2StlObjekt.m
@@ -2,18 +2,21 @@ function [] = FD2StlObjekt( DStlObjekt,fv,farben,RadiusMax)
 %Funktion, die das Stl-objekt darstellt
 
 if DStlObjekt==1
-    hold on %Schreibe folgende Grafikbefehle in das geöffnete Grafikfenster
+    hold on %Schreibe folgende Grafikbefehle in das geÃ¶ffnete Grafikfenster
     alpha(patch(fv,'FaceColor',farben,'EdgeColor','none','FaceLighting','gouraud','AmbientStrength',0.15),0.35);
     %patch(fv,'FaceColor',farben,'EdgeColor','none','FaceLighting','gouraud','AmbientStrength', 0.15,'FaceAlpha',0.35)
     set (gca,'Ydir','reverse');
-    %camlight(0,180); %Lichtquelle hinzufügen
-    material('dull'); %Obeflächen matt
+    %camlight(0,180); %Lichtquelle hinzufÃ¼gen
+    material('dull'); %ObeflÃ¤chen matt
     daspect([1 (360/(2*pi*RadiusMax)) 1]) %Achsenskalierung von X- und Y-Achse
-    axis tight %DarstellungsFeld so nahe am 3d Objekt wie möglich
-    %set(gcf, 'color', [1 1 1]) %figure color (gcf->figure settings)
-    %set(gca, 'color', [1 1 1]) %axis color (gca->axis settings)
-    %set(gca, 'Clipping', 'off');
-    setAxes3DPanAndZoomStyle(zoom(gca),gca,'camera')
+    axis tight %DarstellungsFeld so nahe am 3d Objekt wie mÃ¶glich
+    versiontext=version;
+    if str2double(versiontext(1:3))>=7.9 %ab 2014b neue Grafikplots
+        %set(gcf, 'color', [1 1 1]) %figure color (gcf->figure settings)
+        %set(gca, 'color', [1 1 1]) %axis color (gca->axis settings)
+        %set(gca, 'Clipping', 'off');
+        setAxes3DPanAndZoomStyle(zoom(gca),gca,'camera');
+    end
 end
     
 end
