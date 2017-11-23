b1aa44094797f22e2fc6873cf3a45edc969f5968
Hannes333 <Johannesgysel@gmx.ch>
2017-11-23 16:07:03 +0100

    Update HauptprogrammZyl.m

--- HauptprogrammZyl.m
+++ HauptprogrammZyl.m
@@ -1,41 +1,41 @@
-%Dieses Skript führt die gesammte CAM Bahnberechnung durch 
-%!!! für Zylindrische Werkstücke !!! 
-%Für jeden Bearbeitungsschritt wird die entsprechende Teilfunktion aufgerufen
+%Dieses Skript fÃ¼hrt die gesammte CAM Bahnberechnung durch 
+%!!! fÃ¼r Zylindrische WerkstÃ¼cke !!! 
+%FÃ¼r jeden Bearbeitungsschritt wird die entsprechende Teilfunktion aufgerufen
 %Die Teilfunktione beginnen mit dem Buchsstaben F
-%Für die Entwicklung von neuem Programmcode, empfielt sich zuerst nur mit diesem
-%Skript ohne Benutzeroberfläche zu arbeiten, weil es übersichtlicher ist.
+%FÃ¼r die Entwicklung von neuem Programmcode, empfielt sich zuerst nur mit diesem
+%Skript ohne BenutzeroberflÃ¤che zu arbeiten, weil es Ã¼bersichtlicher ist.
 %In einem zweiten Schritt, kann dann der Programmcode in die
-%Benutzeroberfläche integriert werden
-%Das Skript zur Benutzeroberfläche lautet LaserCAM.m
+%BenutzeroberflÃ¤che integriert werden
+%Das Skript zur BenutzeroberflÃ¤che lautet LaserCAM.m
 
 clc %Bereinigt das Command Window
-clear all %Löscht alle Variabeln
+clear all %LÃ¶scht alle Variabeln
 close all %Vernichtet offene Grafikfenster
 format long g %Nicht wissenschaftliche Schreibweise
 
-Schichtdicke=0.1; %Höhenabstand der Schichten in [mm]
+Schichtdicke=0.1; %HÃ¶henabstand der Schichten in [mm]
 
-Auswahlhoehen=0; %Auswahl der Bearbeitungshöhen (1=ja) (0=nein)
-Auswahlhoeheoben=5; %Auswahlhöhe Oben
-Auswahlhoeheunten=4.9; %Auswahlhöhe Unten
+Auswahlhoehen=0; %Auswahl der BearbeitungshÃ¶hen (1=ja) (0=nein)
+Auswahlhoeheoben=5; %AuswahlhÃ¶he Oben
+Auswahlhoeheunten=4.9; %AuswahlhÃ¶he Unten
 
 Linienabstand=0.5; %Abstand zwischen den Laserbahnen[mm]
 Scangeschw=500; %Einstellen der Scangeschwindigkeit [mm/s]
-VorschubAmax=36000; %Maximale Drehzahl der Drehachse A [°/s]
-MinJumplengthY=0.2; %Minimale Länge der Jumplinie in Y-Richtung [mm]
-DrehoffsetStart=20; %Startdrehwinkel zur ersten Schraffuren [°]
-Drehoffset=10; %Drehwinkel zwischen den Schraffuren [°]
-SchraffurSkywritestart=1; %Skywritelänge zur Beschleunigung der Spiegel und Galvamotoren [mm]
-SchraffurSkywriteend=1; %Skywritelänge zur Abbremsung der Spiegel und Galvamotoren [mm]
-Modus=1; %Startwinkel (1=kleinsmöglicher Winkel) (2=freiwählbarer Winkel) (3=grösstmöglicher Winkel)
-Verhaeltnis=0.6; %Verhältnis zwischen Markierlinie zu Jumplinie bei Startwinkelberechnung nach Modus 1
+VorschubAmax=36000; %Maximale Drehzahl der Drehachse A [Â°/s]
+MinJumplengthY=0.2; %Minimale LÃ¤nge der Jumplinie in Y-Richtung [mm]
+DrehoffsetStart=20; %Startdrehwinkel zur ersten Schraffuren [Â°]
+Drehoffset=10; %Drehwinkel zwischen den Schraffuren [Â°]
+SchraffurSkywritestart=1; %SkywritelÃ¤nge zur Beschleunigung der Spiegel und Galvamotoren [mm]
+SchraffurSkywriteend=1; %SkywritelÃ¤nge zur Abbremsung der Spiegel und Galvamotoren [mm]
+Modus=1; %Startwinkel (1=kleinsmÃ¶glicher Winkel) (2=freiwÃ¤hlbarer Winkel) (3=grÃ¶sstmÃ¶glicher Winkel)
+Verhaeltnis=0.6; %VerhÃ¤ltnis zwischen Markierlinie zu Jumplinie bei Startwinkelberechnung nach Modus 1
 Schraffurwinkelstart=5; %Richtungswinkel der ersten Schraffur [Grad]
 Schraffurwinkelinkrem=0; %Richtungswinkel der darauf folgenden Schraffuren [Grad]
-WinkelAnpassung=0; %Winkel anpassen damit maximale Drehzahl und Scangeschwindigkeit nicht überschritten und kleinstmöchlier Winkel nicht unterschritten wird (1=ja) (0=nein)
+WinkelAnpassung=0; %Winkel anpassen damit maximale Drehzahl und Scangeschwindigkeit nicht Ã¼berschritten und kleinstmÃ¶chlier Winkel nicht unterschritten wird (1=ja) (0=nein)
 OnDelayLength=Scangeschw*0.000015; %Verschiebung der Startpunkte [mm] (l_on = v_s*t_on)
 OffDelayLength=Scangeschw*0.000015; %Verschiebung der Startpunkte [mm] (l_off = v_s*t_off)
-MinimalLaenge=0.1; %Minimale Hatchsegmentlänge [mm]
-%MinimalLaenge=Scangeschw*(0.000015-0.000015); %Minimale Hatchsegmentlänge [mm]
+MinimalLaenge=0.1; %Minimale HatchsegmentlÃ¤nge [mm]
+%MinimalLaenge=Scangeschw*(0.000015-0.000015); %Minimale HatchsegmentlÃ¤nge [mm]
 AxialRichtung=3;    %(Von links nach rechts=1 ,von rechts nach links=2, alternieren=3)
 
 DStlObjekt1=1; %Soll das zylindrische Stl-Objekt Dargestellt werden? (1=ja) (0=nein)
@@ -66,7 +66,7 @@ NCText.Fokus2=' A';
 NCText.Fokus3=' Z';
 NCText.Fokus4='';
 NCText.Vorschub1='F';
-NCText.Vorschub2=' //Dominante Geschw [°/s]';
+NCText.Vorschub2=' //Dominante Geschw [Â°/s]';
 NCText.Eilgang1='G00 X';
 NCText.Eilgang2=' A';
 NCText.Eilgang3='';
@@ -104,23 +104,23 @@ disp('Stl-Objekt eingelesen');
 RadiusMax=max((v(:,2).^2+v(:,3).^2).^0.5);
 v1=v;
 v1(:,2)=v1(:,2)*-(360/(2*pi*RadiusMax)); %Anpassung der orignal Stldatei an die Skalierung in Grad der Y-Richtung
-fv1.vertices=v1; %v enthält die Koordinaten der Eckpunkte
+fv1.vertices=v1; %v enthÃ¤lt die Koordinaten der Eckpunkte
 fv1.faces=f; %f sagt, welche drei Eckpunkte aus v ein Dreieck bilden
 FD2StlObjekt(DStlObjekt1,fv1,[0.2 0.2 0.8],RadiusMax);
 view([45 45]); %Set a nice view angle
 
 %Funktion, die die Stl-Datei von Kartesisch- in Zylinderkoordinaten umwandelt
 [f2,v2] = F03_Transformation(f,v);
-disp('Transformation durchgeführt');
+disp('Transformation durchgefÃ¼hrt');
 
-%Abgerollte geometrie als slt-Datei speichern (für Debugging von F03)
+%Abgerollte geometrie als slt-Datei speichern (fÃ¼r Debugging von F03)
 stlwrite('Abgerollt.stl',f2,v2);
 
 %Darstellung des Stl-Objekts nach der Koordinatentrasformation
-fv2.vertices=v2; %v enthält die Koordinaten der Eckpunkte
+fv2.vertices=v2; %v enthÃ¤lt die Koordinaten der Eckpunkte
 fv2.faces=f2; %f sagt, welche drei Eckpunkte aus v ein Dreieck bilden
 FD2StlObjekt(DStlObjekt2,fv2,[0.2 0.8 0.8],RadiusMax);
-camlight(0,0); %Lichtquelle hinzufügen
+camlight(0,0); %Lichtquelle hinzufÃ¼gen
 
 %Funktion, die die Schnitthoehen berechnet wird aufgerufen
 [Schnitthoehen,Zoben,Zunten]=F05_Schnitthoehen(v2,Schichtdicke,Auswahlhoehen,Auswahlhoeheoben,Auswahlhoeheunten);
@@ -143,7 +143,7 @@ FD2Schnittkontur(DKontur,d,Konturen,RadiusMax);
 [Schraffuren,VorschubDominant,Winkel]=F41_Schraffuren( Konturen,Linienabstand,SchraffurSkywritestart,SchraffurSkywriteend,Schraffurwinkelstart,Schraffurwinkelinkrem,Verhaeltnis,Scangeschw,VorschubAmax,Drehoffset,DrehoffsetStart,Modus,MinJumplengthY,WinkelAnpassung,MinimalLaenge,OnDelayLength,OffDelayLength,AxialRichtung); 
 disp('Schraffuren berechnet');
 
-%Abschätzung der Bearbeitungszeit
+%AbschÃ¤tzung der Bearbeitungszeit
 WinkelStart=0;
 Bearbeitungszeit=0;
 for k=1:size(Schraffuren,1) %Index, der durch die Ebenen von Konturen geht
@@ -153,7 +153,7 @@ for k=1:size(Schraffuren,1) %Index, der durch die Ebenen von Konturen geht
         WinkelStart=WinkelEnd;
     end
 end
-disp(['Geschätze Bearbeitungszeit: ',num2str(round(Bearbeitungszeit/60)),'min ',num2str(mod(Bearbeitungszeit,60),'%4.1f'),'s']);
+disp(['GeschÃ¤tze Bearbeitungszeit: ',num2str(round(Bearbeitungszeit/60)),'min ',num2str(mod(Bearbeitungszeit,60),'%4.1f'),'s']);
 
 %Darstellung der Schraffuren
 FD2Schraffur(DSchraffur,d,Schraffuren,RadiusMax);
@@ -162,115 +162,13 @@ FD2Schraffur(DSchraffur,d,Schraffuren,RadiusMax);
 F51_NCCode(Schraffuren,Titel,NCText,VorschubDominant);
 disp('NC-Code erstellt');
 
+%% Darstellung einer neuen Ebene 
 %{
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PSO Ramsch %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Umrechnung der Schraffurwinkel in Ticks
-PSOArray=cell(size(Schraffuren));
-PSOEntry=zeros(size(PSOArray));
-for k=1:size(Schraffuren,1)
-    if ~isempty(Schraffuren{k,1})
-        PSOArray{k,1}=round(Schraffuren{k,1}(2:end-1,2)*(100000/360));
-        PSOEntry(k,1)=size(PSOArray{k,1},1);
-        if floor(PSOArray{k,1}(1,1)/2^31)<floor(PSOArray{k,1}(end,1)/2^31)
-            disp(['Ebene: ',int2str(k),' wird wegen Aerotechproblemen behandelt']);
-            %PSOArray{k,1}=[];
-            %PSOEntry(k,1)=0;
-            %Nötige Umdrehungen für Aufschiebung werden berechnet und aufaddiert
-            uebergang=floor(PSOArray{k,1}(end,1)/2^31)*2^31;
-            differenz=uebergang-PSOArray{k,1}(1,1); 
-            umdrehungen=ceil(differenz/100000);
-            PSOArray{k,1}(:,1)=PSOArray{k,1}(:,1)+umdrehungen*100000;
-            for j=k:size(Schraffuren,1) 
-                Schraffuren{j,1}(:,2)=Schraffuren{j,1}(:,2)+umdrehungen*360;
-            end
-        end
-    end
-end
-AnzahlPSOEntry=sum(PSOEntry);
-
-%NC-Code Erstellung für PSO
-fid = fopen(Titel, 'W'); %Ein neues txt-file wird geöffnet 
-fprintf(fid,['//Array Stuff','\r\n']);
-fprintf(fid,['DVAR $global[',int2str(AnzahlPSOEntry),']','\r\n']);
-%PSOArray in file ausschreiben
-PSOArrayString=['$global[','%1.0f',']=','%1.0f','\r\n'];
-PSOArrayIndex=0;
-for k=1:size(PSOArray,1)
-    if ~isempty(PSOArray{k,1})
-        for h=1:size(PSOArray{k,1},1)
-            fprintf(fid,PSOArrayString,[PSOArrayIndex,PSOArray{k,1}(h,1)]);
-            PSOArrayIndex=PSOArrayIndex+1;
-        end
-    end
-end
-fprintf(fid,['ARRAY A WRITE $global[0] 0 ',int2str(AnzahlPSOEntry),'\r\n']);
-fprintf(fid,['\r\n']);
-
-fprintf(fid,['//Startposition','\r\n']);
-fprintf(fid,['G90','\r\n']);
-fprintf(fid,['G359','\r\n']);
-fprintf(fid,['VELOCITY ON','\r\n']);
-fprintf(fid,['POSOFFSET CLEAR X Y Z','\r\n']);
-fprintf(fid,['G01 Z(-17.2) X(61.6) Y(-21.715) F10','\r\n']);
-fprintf(fid,['POSOFFSET SET X0 Y0 Z0','\r\n']);
-fprintf(fid,['HOME A','\r\n']);
-fprintf(fid,['\r\n']);
-
-fprintf(fid,['//PSO Start','\r\n']);
-fprintf(fid,['PSOCONTROL A RESET','\r\n']);
-fprintf(fid,['PSOOUTPUT A CONTROL 0 1','\r\n']);
-fprintf(fid,['PSOWINDOW A 1 INPUT 2','\r\n']);
-fprintf(fid,['PSOWINDOW A 1 RANGE ARRAY 0 ',int2str(AnzahlPSOEntry),'\r\n']);
-fprintf(fid,['PSOOUTPUT A WINDOW','\r\n']);
-fprintf(fid,['PSOCONTROL A ARM','\r\n']);
-fprintf(fid,['\r\n']);
-
-%CodeZeilen um die Bearbeitungshöhe anzusteuern wird zusammengesetzt
-FokusString=[NCText.Fokus1,'%4.4f',NCText.Fokus2,'%4.4f',NCText.Fokus3,'%4.4f',NCText.Fokus4,'\r\n'];
-%CodeZeilen um den Vorschub der Dominanten Achse anzugeben wird zusammengesetzt
-VorschubString=[NCText.Vorschub1,'%4.4f',NCText.Vorschub2,'\r\n'];
-%CodeZeilen für eine Laserbearbeitungslinie wird zusammengesetzt
-LaserString=[NCText.Laser1,'%4.4f',NCText.Laser2,'%4.4f',NCText.Laser3,'\r\n']; 
-%CodeZeilen für die Auskommentierung wird zusammengesetzt
-KommentarString1=[NCText.Kommentar1,'################### Ebene %1.0f ####################',NCText.Kommentar2,'\r\n'];
-
-bar = waitbar(0,'NC-Code wird berechnet...'); %Ladebalken erstellen
-fprintf(fid,['//NC-Code','\r\n']);
-for k=1:size(Schraffuren,1)
-    if ~isempty(Schraffuren{k,1})
-        %Kommentar mit Ebenenbenennung
-        fprintf(fid,KommentarString1,k);
-        %CodeZeile zum Einstellen des Vorschubs der Dominanten Drehachse
-        fprintf(fid,VorschubString,VorschubDominant(k));
-        %Mit der Z-Achse den Fokus einstellen
-        SchnittHoehe=Schraffuren{k}(1,3);
-        Korrekturwert=abs(SchnittHoehe); %Bei zylindrischer Bearbeitung
-        %Erster Punkt der Schraffur wird angefahren inklusive Fokuseinstellung
-        fprintf(fid,FokusString,[Schraffuren{k}(1,1:2),Korrekturwert]); 
-        %Letzer Punkt der Schraffur wird angefahren 
-        fprintf(fid,LaserString,Schraffuren{k}(end,1:2)); 
-    end
-    waitbar(k/size(Schraffuren,1)) %Aktualisierung Ladebalken
-end
-close(bar) %Ladebalken schliessen
-fprintf(fid,['\r\n']);
-
-fprintf(fid,['//PSO Ende','\r\n']);
-fprintf(fid,['PSOWINDOW A 1 OFF','\r\n']);
-fprintf(fid,['PSOCONTROL A OFF','\r\n']);
-fprintf(fid,['FREERUN A STOP','\r\n']);
-fprintf(fid,['END PROGRAM','\r\n']);
-
-fclose(fid); %txt-file wird geschlossen
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PSO Ramsch Ende %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%}
-
-%% Darstellung einer neuen Ebene
 cla
 d=1; %Ebene die dargestellt werden soll
 %FD2StlObjekt(DStlObjekt1,fv1,[0.2 0.2 0.8],RadiusMax); %Erstellt ein neues Fenster...
 FD2StlObjekt(DStlObjekt2,fv2,[0.2 0.8 0.8],RadiusMax);
-camlight(0,0); %Lichtquelle hinzufügen
+camlight(0,0); %Lichtquelle hinzufÃ¼gen
 FD2Schnittkontur(DKontur,d,Konturen,RadiusMax);
 FD2Schraffur(DSchraffur,d,Schraffuren,RadiusMax);
 %}
